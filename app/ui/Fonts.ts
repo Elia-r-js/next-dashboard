936d1b4475606f66bcbff9deed4bb8d3eba44818
Ilya <eliababajanybussines@gamil.com>
2025-11-14 00:39:02 +0330

    fix font import

--- app/ui/Fonts.ts
+++ app/ui/Fonts.ts
@@ -1,8 +1,8 @@
-import { Inter } from "next/font/google";
-
-export const inter = Inter({ subsets: ["latin"] });
-
-export const lusitana = Inter({
-  weight: ["700", "400"],
-  subsets: ["Lusitana"],
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
 });
