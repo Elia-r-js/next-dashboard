216ba64c3531482de91eaf4b5d66914cc0cebaac
Ilya <eliababajanybussines@gamil.com>
2025-11-20 17:02:29 +0330

    second

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,13 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
+// import { usePathname } from "next/navigation";
 
+const link =[
+  {name:'Home' , href:'/dashboard'}
+]
 export default function Page() {
+  // const pathname =usePathname()
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -13,6 +18,13 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+          {link.map((item)=>
+         <Link 
+          key={item.name}
+          href={item.href}
+         className={ "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-1 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"}>
+           <p>dashboard</p>
+           </Link>)}
           <p
             className={`${lusitana.className}text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
@@ -38,11 +50,11 @@ export default function Page() {
             alt="screenShot of dashboard project pa"
           />
           <Image
-          src="/hero-mobile.png"
-          height={620}
-          width={560}
-          className="block sm:hidden"
-          alt="you should see this in mobile"
+            src="/hero-mobile.png"
+            height={620}
+            width={560}
+            className="block sm:hidden"
+            alt="you should see this in mobile"
           />
         </div>
       </div>
